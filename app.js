cb7edb1f5544ae415bca20ad683b92a39af7b962
sharopcha <napster1202@gmail.com>
2020-08-25 08:12:30 +0300

    passport google auth

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const passport = require('passport');
+const session = require('express-session');
+
+// Load config
+dotenv.config({ path: './config/config.env' });
+
+// Passport config
+require('./config/passport')(passport);
+
+connectDB();
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+// Session middleware
+app.use(
+  session({
+    secret: 'napster',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
